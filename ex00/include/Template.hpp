13b76f3e44c3b0d4a20c38319839b0b10aa8a1b1
Andrei Kretov <akretov@student.42prague.com>
2025-02-09 19:36:53 +0100

    ex00, introduction to templates

---
+++ ex00/include/Template.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+
+
+template <typename T>
+void swap(T &first_arg, T &second_arg) {
+    T temp;
+
+    temp = first_arg;
+    first_arg = second_arg;
+    second_arg = temp;
+};
+
+template <typename T>
+const T &min(const T &first_arg,const T &second_arg){
+    if (first_arg < second_arg)
+        return first_arg;
+    return second_arg;
+}
+
+template <typename T>
+const T &max(const T &first_arg, const T &second_arg){
+    if (first_arg > second_arg)
+        return first_arg;
+    return second_arg;
+}
+
---
+++ ex01/include/iter.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+void    iter(const T &address_of_array, size_t length, )
